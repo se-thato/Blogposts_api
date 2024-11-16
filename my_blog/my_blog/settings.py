6390838fb8bba6769d40c293de6342afec4f1db5
se-thato <thatoselepe80@gmail.com>
2024-11-16 02:34:15 +0000

    changes

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dm2^2%yp*01+ue=i7)citculk58-2)$=57nq2)y=a%*htt(ybw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'thatoselepe11.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
